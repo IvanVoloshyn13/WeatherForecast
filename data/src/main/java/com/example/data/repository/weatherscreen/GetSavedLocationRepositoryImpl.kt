30212257962df98037a589c302ae0b0396f6f9ef
Ivan Voloshyn <ivan.voloshyn.fs@gmail.com>
2024-01-08 14:58:30 +0100

    Rewrite function that was getting saved locations from local database using Resource helper class in returen type for better error handling

--- data/src/main/java/com/example/data/repository/weatherscreen/GetSavedLocationRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/weatherscreen/GetSavedLocationRepositoryImpl.kt
@@ -4,15 +4,23 @@ import com.example.data.repository.searchscreen.toSearchedCity
 import com.example.data.storage.room.AppDatabase
 import com.example.domain.models.SearchedCity
 import com.example.domain.repository.weather.GetSavedLocationRepository
+import com.example.domain.utils.Resource
+import java.io.IOException
 import javax.inject.Inject
 
 class GetSavedLocationRepositoryImpl @Inject constructor(
     private val database: AppDatabase
 ) : GetSavedLocationRepository {
-    override suspend fun getSavedCityList(): List<SearchedCity> {
-        val cityListEntity = database.getCityDao().getAllCities()
-        return cityListEntity.map {
-            it.toSearchedCity()
+    override suspend fun getSavedCityList(): Resource<List<SearchedCity>> {
+        return try {
+            val cityListEntity = database.getCityDao().getAllCities()
+            val searchedCities = cityListEntity.map {
+                it.toSearchedCity()
+            }
+            Resource.Success(data = searchedCities)
+        } catch (e: IOException) {
+            Resource.Error(e = e, message = e.message)
         }
+
     }
 }
